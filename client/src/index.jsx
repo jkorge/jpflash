98ae6e88cfec1d763dd6b60743c13bf16809878a
James Korge <james@korgetech.com>
2025-06-04 14:23:44 -0400

    New layout and additional functionality including card sets and example audio

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,12 +1,7 @@
 import { createRoot } from 'react-dom/client';
 
-import { HomePage } from './home.jsx';
 import { KanjiPage } from './kanji.jsx';
-import { NavBar } from './nav.jsx';
 import { render, navigate } from './util.jsx';
-
-// Render the navigation menu
-render('banner', <NavBar/>);
 
 // Navigate to the home page
 navigate(<KanjiPage/>);
--- client/src/kanji.jsx
+++ client/src/kanji.jsx
@@ -1,13 +1,19 @@
-import { useState, useEffect, useRef } from 'react';
+import { useState, useEffect, useRef, createPortal } from 'react';
 import { api_url } from './util.jsx';
 import '../styles/kanji.css';
 
 const kroot = `${api_url}/kanji`;
-const default_kanji = '先生安飲雨';
+const default_kanji = '漢字';
 
 async function fetchKanji(k) {
     const res = await fetch(`${kroot}/${k}`);
     const data = await res.json();
+    return data;
+}
+
+async function fetchSet(name) {
+    const res = await fetch(`${kroot}/set/${name}`);
+    const data = await res.text();
     return data;
 }
 
@@ -30,7 +36,7 @@ function VideoContainer({k, play}) {
 
     return (
         <div className='videoContainer'>
-            <video onEnded={handleEnded} muted src={`${kroot}/anim/${k}`} ref={ref}></video>
+            <video onEnded={handleEnded} muted src={`${kroot}/anim/${k}`} preload='none' ref={ref}></video>
         </div>
     );
 }
@@ -49,8 +55,8 @@ function DataItem({name, info}) {
 
     let idx = -1;
     const listItems = (info.split(split_on)).map((x) => {
-        idx += 1;
-        if (lang == null){
+        idx++;
+        if (lang == null || (name == 'Radical' && idx == 2)) {
             return <li key={idx}>{x}</li>;
         } else {
             return <li key={idx} lang={lang}>{x}</li>;
@@ -72,23 +78,33 @@ function DataItem({name, info}) {
 
 function DataContainer({data}) {
 
+    const rad_data = `${data.radical}、${data.rad_name_ja}、${data.rad_meaning}`;
+
     return (
         <div className='dataContainer'>
             <div className='dataGrid'>
                 <DataItem name='Meaning' info={data.kmeaning ?? ''} />
                 <DataItem name='Kunyomi' info={data.kunyomi_ja ?? ''} />
                 <DataItem name='Onyomi' info={data.onyomi_ja ?? ''} />
-                <DataItem name='Radical' info={data.radical ?? ''} />
+                <DataItem name='Radical' info={rad_data ?? ''} />
             </div>
         </div>
     );
 }
 
-function Example({ex}) {
+function Example({k, idx, ex}) {
     const [ja, eng] = ex;
     const [kanji_text, kana_text] = ja.split('（');
+
+    function handleAudioIconClick(event) { event.target.firstElementChild.play(); }
+
     return (
         <tr>
+            <td>
+                <i className='fa fa-volume-up' onClick={handleAudioIconClick}>
+                    <audio preload='none' src={`${kroot}/audio/${k}/${idx}`}></audio>
+                </i>
+            </td>
             <td lang='jp'>{kanji_text}</td>
             <td lang='jp'>{kana_text.slice(0, -1)}</td>
             <td>{eng}</td>
@@ -97,12 +113,12 @@ function Example({ex}) {
 
 }
 
-function ExamplesContainer({examples}) {
+function ExamplesContainer({k, examples}) {
 
     let idx = -1;
     const rows = examples.map((ex) => {
-        idx += 1;
-        return <Example key={idx} ex={ex ?? ['（', '']} />;
+        idx++;
+        return <Example key={idx} k={k} idx={idx} ex={ex ?? ['（', '']} />;
     });
 
     return (
@@ -112,6 +128,7 @@ function ExamplesContainer({examples}) {
                 <table className='examples'>
                     <thead>
                         <tr>
+                            <th scope="col"></th>
                             <th scope="col">Kanji</th>
                             <th scope="col">Kana</th>
                             <th scope="col">English</th>
@@ -134,7 +151,7 @@ function CardBack({data, playVideo}) {
             <h2 lang='jp'>{data.kanji}</h2>
             <VideoContainer k={data.kanji} play={playVideo} />
             <DataContainer data={data} />
-            <ExamplesContainer examples={data.examples ?? []} />
+            <ExamplesContainer k={data.kanji} examples={data.examples ?? []} />
 
         </div>
     );
@@ -162,7 +179,7 @@ function CardContainer({data, showFront, visible}) {
     return (
             <div className='cardContainer' style={style}>
                 <CardFront data={data}/>
-                <CardBack data={data} playVideo={!showFront} />
+                <CardBack data={data} playVideo={visible && !showFront} />
             </div>
     );
 }
@@ -222,7 +239,7 @@ function CardStack({kanji}) {
     // Construct cards
     let idx = -1;
     const cards = data.map((x) => {
-        idx += 1
+        idx++;
         return <CardContainer key={idx} data={x} showFront={showFront} visible={showCardNum == idx} />;
     });
 
@@ -238,28 +255,98 @@ function CardStack({kanji}) {
     );
 }
 
-function SearchBar({inputDefault, onSubmit}) {
+function SearchModal({onSubmit, ref}) {
     return (
-        <form className='searchBar' onSubmit={onSubmit}>
-            <input lang='jp' type='text' defaultValue={inputDefault} name='kanjiInput' className='searchInput'></input>
-            <button className='searchButton' type='submit'><i className='fa fa-search'></i></button>
-        </form>
+        <dialog className='searchModal' closedby='any' ref={ref}>
+            <form method='dialog' className='searchBar' onSubmit={onSubmit}>
+                <input lang='jp' type='text' placeholder={default_kanji} name='kanjiInput' className='searchInput'></input>
+                <button className='searchButton' type='submit'><i className='fa fa-search'></i></button>
+            </form>
+        </dialog>
     )
+}
+
+function SetSelectionModal({onChange, ref}){
+
+    let idx = -1;
+    const gradeList = [1,2,3,4,5,6].map((x) => {
+        idx++;
+        return <option key={idx} value={`grade-${x}`}>{`Grade ${x}`}</option>
+    });
+    const genkiChapters = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23].map((x) => {
+        idx++;
+        return <option key={idx} value={`genki-${x}`}>{`Ch. ${x}`}</option>
+    })
+
+    return (
+        <dialog className='setSelectionModal' closedby='any' ref={ref}>
+            <form method='dialog'>
+                <select className='setList' name='selectedSet' size='10' onChange={onChange}>
+                    {/*<option value='n2'>All N2</option>*/}
+                    <optgroup label='Grade Level'>
+                        {gradeList}
+                    </optgroup>
+                    <optgroup label='Genki Chapters'>
+                        {genkiChapters}
+                        {/*<option key={idx} value='genki-all'>All</option>*/}
+                    </optgroup>
+                </select>
+            </form>
+        </dialog>
+    );
 }
 
 function KanjiPage() {
 
     const [kanji, setKanji] = useState(default_kanji);
+    const searchModalRef = useRef(null);
+    const setSelectionModalRef = useRef(null);
 
-    function handleSearchSubmit(event){
-        event.preventDefault();
+    // Modals
+    function handleSearchSubmit(event) {
         const formData = new FormData(event.target);
         setKanji(formData.get('kanjiInput') ?? '');
     }
 
+    function handleSetSelection(event) {
+        const form = event.target.parentElement;
+        form.submit();
+        const formData = new FormData(form);
+        async function runFetchSet() {
+            const res = await fetchSet(formData.get('selectedSet'));
+            setKanji(res);
+        }
+        runFetchSet();
+    }
+
+    const setSelectionModal = <SetSelectionModal onChange={handleSetSelection} ref={setSelectionModalRef} />
+    const searchModal = <SearchModal onSubmit={handleSearchSubmit} ref={searchModalRef} />
+
+    // Button handlers
+    function handleChooseSetButtonClick(event) { setSelectionModalRef.current.showModal() }
+    function handleSearchButtonClick(event) { searchModalRef.current.showModal(); }
+    function handleShuffleButtonClick(event) {
+        let arr = kanji.split('');
+        let idx = arr.length;
+
+        while (idx != 0) {
+            let random = Math.floor(Math.random() * idx);
+            idx--;
+            [arr[idx], arr[random]] = [arr[random], arr[idx]];
+        }
+
+        setKanji(arr.join(''));
+    }
+
     return (
         <>
-            <SearchBar inputDefault={default_kanji} onSubmit={handleSearchSubmit} />
+            <div className='buttonArray mainButtonArray'>
+                <button className='shuffleButton' onClick={handleShuffleButtonClick}>Shuffle</button>
+                <button className='chooseSetButton' onClick={handleChooseSetButtonClick}>Choose Set</button>
+                <button className='searchButton' onClick={handleSearchButtonClick}>Search</button>
+            </div>
+            {setSelectionModal}
+            {searchModal}
             <CardStack kanji={kanji} />
         </>
     )
