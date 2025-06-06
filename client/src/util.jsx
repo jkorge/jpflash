ad7b91f9f6c29375e3eeaad24bb3d27038c25021
James Korge <james@korgetech.com>
2025-06-06 15:19:49 -0400

    Removed dev api server reference to localhost

--- client/src/util.jsx
+++ client/src/util.jsx
@@ -1,7 +1,6 @@
 import { createRoot } from 'react-dom/client';
 
-// const api_url = 'https://jkorge.pythonanywhere.com';
-const api_url = 'http://localhost:5000';
+const api_url = 'https://jkorge.pythonanywhere.com';
 const content = createRoot(document.getElementById('content'));
 
 function render(id, component) {
